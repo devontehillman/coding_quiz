bcf1ccce34553e848e983bf8bc4b44cd929c40e2
Devonte Hillman <hillmand@mail.gvsu.edu>
2020-12-23 23:46:13 -0500

    down size to so that I can manage all and refine web pages

--- assets/highscores.js
+++
@@ -1,7 +0,0 @@
-const highScoreslist = document.querySelector('#highScoreslist')
-const highScores = JSON.parse(localStorage.getItem('highScores'))|| []
-
-highScoreslist.innerHTML = 
-highScores.map(score => {
-    return`<li class = "high=score"> ${score.name} - ${score.score} </li>`
-}).join('')
