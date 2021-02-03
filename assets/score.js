6c0be106e9da1c4ad22a9c3606f20ade49cebd1b
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-02-02 19:33:06 -0500

    updated score page

--- assets/score.js
+++ assets/score.js
@@ -1,35 +1,160 @@
-const username = document.querySelector('#username');
-const saveScoreBtn = document.querySelector('#saveScoreBtn');
-const finalScore = document.querySelector('finalScore');
+//const username = document.querySelector('#userName').value;
+//const saveScoreBtn = document.querySelector('#saveScoreBtn');
+//const finalScore = document.querySelector('#scoreArea');
 const mostRecentScores = document.querySelector('#mostRecentScore'); 
 
-const highScores = JSON.parse(localStorage.getItem('highscores')) || []
 
-const MAX_HIGH_SCORES = 5 ;
+const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScores
+const highScores = JSON.parse(localStorage.getItem('mostRecentScore'))
 
-username.addEventListener('keyup', () =>{
-    SVGPathSegCurvetoCubicSmoothRel.Btn.disabled = !username.value
+
+$('#scoreArea').text(highScores)
+
+var scores = init();
+console.log(scores)
+renderScores(scores);
+
+
+
+function renderScores(...arry) {
+    // Clear todoList element and update todoCountSpan
+    $('#scoreslist').innerHTML = "";//access list to dispay scores
+    //console.log(scores.length)
+
+    // Render a new li for each score
+    console.log(scores)
+    if (scores === undefined){
+        return
+    }
+    for (var i = 0; i < scores.length; i++) {
+    var score = ("Score:"+scores[i].score+" Name:"+scores[i].name);
+    console.log(score)
+    var li = document.createElement("li");
+    li.textContent = score;
+    li.setAttribute("data-index", i);
+    $('#scoreslist').append(li);
+    }
+}
+
+function init() {
+    // Get stored todos from localStorage
+    // Parsing the JSON string to an object
+    var storedScores = JSON.parse(localStorage.getItem("testarray"));//savehighScores
+    console.log(storedScores)
+    if (storedScores === null) {
+        console.log("Storage empty")
+        return;
+        }
+    else {
+        //renderScores();
+        console.log(storedScores)
+        return (storedScores);
+    }
+    
+}
+
+
+
+//pull in user name and clear area
+// username.addEventListener('keyup', () =>{
+//    saveHighscore(this) 
+// })
+$('#saveScoreBtn').click(function(event){
+    event.preventDefault();
+    console.log("click")
+    const recentScore = JSON.parse(localStorage.getItem('mostRecentScore'))
+    var userName = $('#userName').val()//add a trim
+
+    var obj =  {
+        "score": recentScore,
+        "name": userName
+    }
+    // Return from function early if submitted todoText is blank
+    if (userName === "") {
+    return;
+    }
+
+    // Add new todoText to todos array, clear the input
+    scores.push(obj);
+    console.log(scores)
+    // $('#userName').remove()
+    // $('#scoreArea').remove()
+    
+    if (scores.length > 5) {
+        scores.push(obj);
+        scores.splice(1)
+        var score = ("Score:"+obj.score+" Name:"+obj.name);
+        console.log(score)
+        var li = document.createElement("li");
+        li.textContent = score;
+        
+    $('#scoreslist').append(li);
+    }else {
+        scores.push(obj);
+        var score = ("Score:"+obj.score+" Name:"+obj.name);
+        console.log(score)
+        var li = document.createElement("li");
+        li.textContent = score;
+        
+    }
+    // Store updated todos in localStorage, re-render the list make textarea disapear
+    
+    storeScores();
+
+    // saveHighscore(event)
 })
+
+function storeScores() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem('savehighScores', JSON.stringify(scores));
+    console.log("saved new score")
+}
 
 saveHighscore = e => {
     e.preventDefault()
 
     const score = {
-        socre: mostRecentScore,
-        name : username.value
+        score: highScores,
+        name : $('#userName').val()
     }
+    console.log(score)
+    // console.log(typeof(highScores))
+    // highScores.push(score)
 
-    highScores.push(score)
+    // highScores.sort((a,b) => {
+    //     return b.score - a.score 
+    // })
 
-    highScores.sort((a,b) => {
-        return b.score - a.score 
-    })
+    // highScores.splice(5)
 
-    highScores.splice(5)
-
-    localStorage.setItem(highScores, JSON.stringify(highscores))
+    localStorage.setItem('savehighScores', JSON.stringify(score))
     window.location.assign('/')
 
 }
+
+
+
+// problems 
+// if there is not data stored get a area 
+// need to clear area to add new item 
+
+// // When form is submitted...
+// todoForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
+  
+//     var todoText = todoInput.value.trim();
+  
+//     // Return from function early if submitted todoText is blank
+//     if (todoText === "") {
+//       return;
+//     }
+  
+//     // Add new todoText to todos array, clear the input
+//     todos.push(todoText);
+//     todoInput.value = "";
+  
+//     // Store updated todos in localStorage, re-render the list
+//     storeTodos();
+//     renderTodos();
+//   });
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,17 @@
 const question  = document.querySelector("#question");
 const choices  = Array.from(document.querySelectorAll('.choice-text'));
-const progressText  = document.querySelector('#ProgressText');
+//const progressText  = document.querySelector('#ProgressText');
 const scoreText = document.querySelector('#score');
-const progressBarFull  = document.querySelector('#progressBarFull');
+const progressBarFull  = document.querySelector('#progressBarFull');//not usign this 
 const timerText = document.querySelector('#countDown');
 
-let currentQuestion = {}
-let acceptingAnswers = true 
-let score = 0 
+let currentQuestion = {}//object to store all the questions 
+let acceptingAnswers = true //boolean to singnal page change
+let score = 0 // score counter 
 let questionCounter = 0 
-let availableQuestions = []
+let availableQuestions = [] //Array to keep track of unanswered questions
 
+// creation of questions
 let questions = [
     {
         question:'What is 2 + 2?',
@@ -47,49 +48,49 @@ let questions = [
     }
 ]
 
-const SCORE_POINTS = 1
-const MAX_QUSTIONS = 4
+const SCORE_POINTS = 1 // score increment
+const MAX_QUSTIONS = 4 // number of questions
 
-startGame = () => {
+startGame = () => {// resets score and questions 
     questionCounter = 0 
     score = 0 
-    availableQuestions = [...questions]
+    availableQuestions = [...questions]// use of rest syntax to retrive the remaining should there be a restOfQuestions instead 
     getNewQuestion()
 }
 
 getNewQuestion = () => { 
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUSTIONS || timerDone === true){// stores score of quiz
-        localStorage.setItem("mostRectentScore", score);
-
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUSTIONS){// stores score of quiz and goes to score page
+        localStorage.setItem("mostRecentScore", JSON.stringify(score));
         return window.location.assign('score.html')
     }
-
+    // progress bar
     questionCounter++;
     //progressText.innerText = `Question ${questionCounter} of ${MAX_QUSTIONS}`//changes the count of text 
     //progressBarFull.style.width = `${(questionCounter/MAX_QUSTIONS) * 100}%`// get qustion on and convert to percent
 
-    const  questionIndex = Math.floor(Math.random() * availableQuestions.length); //cycles through questions
-    currentQuestion = availableQuestions[questionIndex]; //Keeps track of question on 
-    question.innerText = currentQuestion.question; // know what question to ask
+    //
+    const  questionIndex = Math.floor(Math.random() * availableQuestions.length); //gets randon number for question
+    currentQuestion = availableQuestions[questionIndex]; //selects random question and stores it in current question
+    question.innerText = currentQuestion.question; //updates html question area with  
 
-    choices.forEach(choice => { // updates choices
-        const number = choice.dataset['number']// so it knows what choice we are clicking
-        choice.innerText = currentQuestion['choice' + number]
+    choices.forEach(choice => { // selects each choice element to updates them
+        const number = choice.dataset['number']// updates each choice with its respective answer
+        choice.innerText = currentQuestion['choice' + number]// updates each choice with its respective answer
     })
 
-    availableQuestions.splice(questionIndex,1);//removes question from index 
+    availableQuestions.splice(questionIndex,1);//removes used question from index 
 
     acceptingAnswers = true;
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) {return}
+        if(!acceptingAnswers) {return} //returns early if questions are done
 
         acceptingAnswers == false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'] // get the number for the index of which button was clicked 
-        console.log(selectedChoice);
+        console.log(selectedChoice);// logs what choice 
 
         let classtoApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'; //turnary???? toggle the style
 
@@ -97,9 +98,9 @@ choices.forEach(choice => {
             incrementScore(SCORE_POINTS);
         }
 
-        selectedChoice.parentElement.classList.add(classtoApply)
+        selectedChoice.parentElement.classList.add(classtoApply)//adds color when right or wrong
 
-        setTimeout(() => { // gives us time to see that the user did select a answer
+        setTimeout(() => { // gives time so that see that the color is displayed
             selectedChoice.parentElement.classList.remove(classtoApply)
             getNewQuestion()// gets next question
 
@@ -111,25 +112,26 @@ incrementScore = num => {
     score +=num
     scoreText.innerText = score; // update the score
 }
-var totalSeconds = 4;
-var timerDone = false;
 
-function setTime(x) {
+var totalSeconds = 500;
+//var timerDone = false;
+
+function setTime() { // need to pass in var to be returned 
     var timerInterval = setInterval(function() {
         totalSeconds--;
         timerText.textContent = totalSeconds;
     
-        if(totalSeconds === 0) {
-          clearInterval(timerInterval);
-          timerDone = true;
-          return  timerDone;
+    if(totalSeconds === 0) {
+        clearInterval(timerInterval);
+        localStorage.setItem("mostRectentScore", JSON.stringify(score));
+        return window.location.assign('score.html')
         }
     }, 1000);
   }
 
 
 
-setTime(timerDone)
+setTime()
 startGame()
 
 
